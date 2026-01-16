ebf9ae8a073f03b0c88446c74775a9f2425893d6
Helix-Synergy <info.helixconferences@gmail.com>
2026-01-16 15:50:52 +0530

    CodeIT

--- vite-project/src/Navbar/Contact.jsx
+++ vite-project/src/Navbar/Contact.jsx
@@ -148,7 +148,7 @@ const Contact = () => {
             <div className="col-xl-5 order-1 order-xl-1">
               <div className="contact-two__map">
                 <img
-                  src="https://html.kodesolution.com/2024/stratify-html/images/contact/map.png"
+                  src="../../public/Images/Contact1.jpg"
                   alt="location map"
                 />
               </div>
